ec610c2603a006ea2683c65ff144288399ecea7f
Grace Lu <155923103+ggracelu@users.noreply.github.com>
2025-05-06 06:12:38 +0000

    done with actors

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,12 @@
+class ActorsController < ApplicationController
+  def index
+    render({ :template => "actor_templates/list"})
+  end
+
+  def show
+    actor_id = params.fetch("actor_id")
+    matching_records = Actor.where({ :id => actor_id})
+    @an_actor = matching_records.at(0)
+    render({ :template => "actor_templates/details"})
+  end
+end
